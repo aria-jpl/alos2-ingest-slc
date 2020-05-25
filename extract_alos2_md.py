59c577f859cf16f0fe5e3acc4380105391f1009a
Mohammed Karim <mkarim@jpl.nasa.gov>
2020-05-25 19:04:36 +0000

    update

--- extract_alos2_md.py
+++ extract_alos2_md.py
@@ -9,16 +9,17 @@ import json
 import re
 import requests
 
-def create_alos2app_xml(dir_name):
-    fp = open('alos2App.xml', 'w')
-    fp.write('<alos2App>\n')
-    fp.write('    <component name="alos2insar">\n')
-    fp.write('        <property name="master directory">{}</property>\n'.format(os.path.abspath(dir_name)))
-    fp.write('        <property name="slave directory">{}</property>\n'.format(os.path.abspath(dir_name)))
-    fp.write('    </component>\n')
-    fp.write('</alos2App>\n')
-    fp.close()
+#!/usr/bin/env python3
+# David Bekaert - Jet Propulsion Laboratory
+# set of functions that are leveraged in the packaging of the ARIA standard product 
 
+from __future__ import division
+from builtins import str
+from builtins import range
+from past.utils import old_div
+import glob
+import os
+import xml_json_converter
 
 def loadProduct(xmlname):
     '''
@@ -27,6 +28,8 @@ def loadProduct(xmlname):
     # from Cunren's code on extracting track data from alos2App
     import isce, isceobj
     from iscesys.Component.ProductManager import ProductManager as PM
+
+    print("loadProduct(xmlname) : {}".format(xmlname))
     pm = PM()
     pm.configure()
     obj = pm.loadProduct(xmlname)
@@ -45,12 +48,48 @@ def loadTrack(date):
         track.frames.append(loadProduct(x))
     return track
 
-def getMetadataFromISCE(track):
-    # from Cunren's code on extracting track data from alos2App
-    import isce, isceobj
-    from isceobj.Alos2Proc.Alos2ProcPublic import getBboxRdr
+def getTrackFrameData(track):
+    '''
+    get frame information 
+    '''
+    import datetime
 
-    #####################################
+    frameData = {}
+
+    numberOfFrames = len(track.frames)
+    numberOfSwaths = len(track.frames[0].swaths)
+
+    rangePixelSizeList = []
+    sensingStartList = []
+    sensingEndList = []
+    startingRangeList = []
+    endingRangeList = []
+    azimuthLineIntervalList =[]
+    azimuthPixelSizeList = []
+    swaths = []
+
+    for i in range(numberOfFrames):
+        for j in range(numberOfSwaths):
+            swath = track.frames[i].swaths[j]
+            swaths.append(swath)
+            rangePixelSizeList.append(swath.rangePixelSize)
+            azimuthLineIntervalList.append(swath.azimuthLineInterval)
+            azimuthPixelSizeList.append(swath.azimuthPixelSize)
+            sensingStartList.append(swath.sensingStart)
+            sensingEndList.append(swath.sensingStart + datetime.timedelta(seconds=(swath.numberOfLines-1) * swath.azimuthLineInterval))
+            startingRangeList.append(swath.startingRange)
+            endingRangeList.append(swath.startingRange + (swath.numberOfSamples - 1) * swath.rangePixelSize)
+    azimuthTimeMin = min(sensingStartList)
+    azimuthTimeMax = max(sensingEndList)
+    azimuthTimeMid = azimuthTimeMin+datetime.timedelta(seconds=(azimuthTimeMax-azimuthTimeMin).total_seconds()/2.0)
+    rangeMin = min(startingRangeList)
+    rangeMax = max(endingRangeList)
+    rangeMid = (rangeMin + rangeMax) / 2.0
+
+    bbox = [rangeMin, rangeMax, azimuthTimeMin, azimuthTimeMax]
+    pointingDirection = {'right': -1, 'left': 1}
+    
+     #####################################
     # in image coordinate
     #         1      2
     #         --------
@@ -68,15 +107,8 @@ def getMetadataFromISCE(track):
     #            --------
     #            3       4
     #####################################
-
-    pointingDirection = {'right': -1, 'left': 1}
-    bboxRdr = getBboxRdr(track)
-    rangeMin = bboxRdr[0]
-    rangeMax = bboxRdr[1]
-    azimuthTimeMin = bboxRdr[2]
-    azimuthTimeMax = bboxRdr[3]
-
     # in image coordinate
+
     # corner 1
     llh1 = track.orbit.rdr2geo(azimuthTimeMin, rangeMin, height=0, side=pointingDirection[track.pointingDirection])
     # corner 2
@@ -98,11 +130,32 @@ def getMetadataFromISCE(track):
         else:
             footprint = [llh3, llh4, llh1, llh2]
 
-    # footprint
-    return footprint, azimuthTimeMin, azimuthTimeMax
+    
+    frameData['numberOfFrames'] = numberOfFrames
+    frameData['numberOfSwaths'] = numberOfSwaths 
+    frameData['rangePixelSizeList'] = rangePixelSizeList
+    frameData['sensingStartList'] = sensingStartList
+    frameData['sensingEndList'] = sensingEndList
+    frameData['startingRangeList'] = startingRangeList
+    frameData['endingRangeList'] = endingRangeList
+    frameData['azimuthLineIntervalList'] = azimuthLineIntervalList
+    frameData['azimuthPixelSizeList'] = azimuthPixelSizeList
+    frameData['bbox'] = bbox
+    frameData['footprint'] = footprint
+    frameData['swaths'] = swaths
+    frameData['rangeMin'] = rangeMin
+    frameData['rangeMax'] = rangeMax
+    frameData['rangeMid'] = rangeMid
+
+    return frameData
 
 
 def get_alos2_obj(dir_name):
+    import os
+    import glob
+    import re
+    from subprocess import check_call, check_output
+
     track = None
     img_file = sorted(glob.glob(os.path.join(dir_name, 'IMG*')))
 
@@ -119,11 +172,30 @@ def get_alos2_obj(dir_name):
 
     return track
 
+def get_alos2_bbox(args):
+    import json
 
-def create_alos2_md_isce(dirname, filename):
+    ref_json_file = args[0]
+    with open (ref_json_file, 'r') as f:
+        data = json.load(f)
+
+    return data['bbox']
+
+    
+def get_alos2_bbox_from_footprint(footprint):
+    bbox = []
+    for i in range(len(footprint)):
+        bbox.append([footprint[i][0], footprint[i][1]])
+    return bbox
+
+def create_alos2_md_json(dirname):
+    from scipy.constants import c
+    from statistics import mean
+
     track = get_alos2_obj(dirname)
-
-    bbox, sensingStart, sensingEnd = getMetadataFromISCE(track)
+    frameData = getTrackFrameData(track)
+    bbox = frameData['footprint']
+    
     md = {}
     md['geometry'] = {
         "coordinates":[[
@@ -135,17 +207,80 @@ def create_alos2_md_isce(dirname, filename):
         ]],
         "type":"Polygon"
     }
-    md['start_time'] = sensingStart.strftime("%Y-%m-%dT%H:%M:%S.%f")
-    md['stop_time'] = sensingEnd.strftime("%Y-%m-%dT%H:%M:%S.%f")
+    md['sensing_start'] = "{}".format(min(frameData['sensingStartList']).strftime("%Y-%m-%dT%H:%M:%S.%f"))
+    md['sensing_stop'] = "{}".format(max(frameData['sensingEndList']).strftime("%Y-%m-%dT%H:%M:%S.%f"))
     md['absolute_orbit'] = track.orbitNumber
     md['frame'] = track.frameNumber
     md['flight_direction'] = 'asc' if 'asc' in track.catalog['passdirection'] else 'dsc'
     md['satellite_name'] = track.spacecraftName
     md['source'] = "isce_preprocessing"
+    md['bbox'] = get_alos2_bbox_from_footprint(bbox)
+    md['pointing_direction'] = track.catalog['pointingdirection']
+    md['radar_wave_length'] = track.catalog['radarwavelength']
+    md['starting_range'] = min(frameData['startingRangeList'])
+    md['azimuth_pixel_size'] = mean(frameData['azimuthPixelSizeList'])
+    md['azimuth_line_interval'] = mean(frameData['azimuthLineIntervalList'])
+    md['frequency'] = old_div(c, track.catalog['radarwavelength'])
+    md['orbit_type'] = get_orbit_type(track.orbit.getOrbitQuality())
+    md['orbit_source'] = track.orbit.getOrbitSource()
+    md['nearRange'] = frameData['rangeMin']
+    md['farRange'] = frameData['rangeMax']
+    md['rangePixelSize'] = mean(frameData['rangePixelSizeList'])
 
+    return md
+
+def get_orbit_type(orbit_quality):
+    if 'precision' in orbit_quality:
+        return "POEORB"
+    return "RESORB"
+
+def create_alos2_md_file(dirname, filename):
+    import json
+    md = create_alos2_md_json(dirname)
+    #print(md)
     with open(filename, "w") as f:
         json.dump(md, f, indent=2)
         f.close()
+
+
+def get_alos2_metadata_variable(args):
+    '''
+        return the value of the requested variable
+    '''
+
+    data = None
+    masterdir = args[0]
+    variable = args[1]
+
+    print("\n\nget_alos2_metadata_variable(args) : {}".format(args))
+
+    alos2_metadata = get_alos2_metadata_reference_json(args[0]) #create_alos2_md_json(masterdir) # get_alos2_metadata(masterdir)
+    if variable in alos2_metadata:
+        data = alos2_metadata[variable]
+
+    return data
+
+def get_alos2_metadata_reference_json(ref_json_file):
+    import json
+
+    data = {}
+    with open (ref_json_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+
+def create_alos2app_xml(dir_name):
+    fp = open('alos2App.xml', 'w')
+    fp.write('<alos2App>\n')
+    fp.write('    <component name="alos2insar">\n')
+    fp.write('        <property name="master directory">{}</property>\n'.format(os.path.abspath(dir_name)))
+    fp.write('        <property name="slave directory">{}</property>\n'.format(os.path.abspath(dir_name)))
+    fp.write('    </component>\n')
+    fp.write('</alos2App>\n')
+    fp.close()
+
+
 
 def create_alos2_md_bos(dir_name, filename):
     img_file = sorted(glob.glob(os.path.join(dir_name, 'IMG*')))
